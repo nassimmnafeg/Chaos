d050ad356fafd45415052ec43481bd9ce69cdabd
Fahmi Laajimi <fahmi.laajimi@canaltp.fr>
2019-05-02 16:43:36 +0200

    Update migration script for impacts history

--- migrations/versions/38f369376261_.py
+++ migrations/versions/38f369376261_.py
@@ -35,14 +35,50 @@ def upgrade():
         schema='history'
     )
     op.create_table('associate_impact_pt_object',
-        sa.Column('impact_id', sa.Integer(), nullable=True),
+        sa.Column('impact_id', postgresql.UUID(), nullable=True),
         sa.Column('pt_object_id', postgresql.UUID(), nullable=True),
         sa.Column('version', sa.Integer(), nullable=False),
         sa.PrimaryKeyConstraint('impact_id', 'pt_object_id', 'version'),
         schema='history'
     )
+    op.execute('CREATE OR REPLACE FUNCTION log_disruption_update() \
+                  RETURNS trigger AS \
+                $BODY$ \
+                BEGIN \
+                 INSERT INTO history.disruption(created_at,updated_at,disruption_id,reference,note,\
+                    start_publication_date,end_publication_date,version,client_id,contributor_id,\
+                    cause_id,status) \
+                 VALUES(OLD.created_at,OLD.updated_at,OLD.id,OLD.reference,OLD.note,OLD.start_publication_date,\
+                    OLD.end_publication_date,OLD.version,OLD.client_id,OLD.contributor_id,\
+                    OLD.cause_id,OLD.status); \
+                 INSERT INTO history.associate_disruption_tag(tag_id,disruption_id,version) \
+                 SELECT tag_id,disruption_id,OLD.version FROM public.associate_disruption_tag WHERE disruption_id = OLD.id; \
+                 INSERT INTO history.associate_disruption_pt_object(disruption_id,pt_object_id,version) \
+                 SELECT disruption_id,pt_object_id,OLD.version FROM public.associate_disruption_pt_object WHERE disruption_id = OLD.id; \
+                 INSERT INTO history.associate_disruption_property(value,disruption_id,property_id,version) \
+                 SELECT value,disruption_id,property_id,OLD.version FROM public.associate_disruption_property WHERE disruption_id = OLD.id; \
+                 \
+                 INSERT INTO history.impact(impact_id,created_at,updated_at,disruption_id,status,severity_id,\
+                    send_notifications,version,notification_date) \
+                 SELECT id,created_at,updated_at,disruption_id,status,severity_id,send_notifications,\
+                    version,notification_date FROM public.impact WHERE disruption_id = OLD.id; \
+                 INSERT INTO history.associate_impact_pt_object(impact_id,pt_object_id,version) \
+                 SELECT impact_id,pt_object_id,OLD.version FROM public.associate_impact_pt_object WHERE impact_id IN (\
+                 SELECT id FROM public.impact WHERE disruption_id = OLD.id); \
+                 \
+                 RETURN NEW; \
+                END; \
+                $BODY$\
+                LANGUAGE plpgsql VOLATILE')
+    op.execute(' DROP TRIGGER IF EXISTS last_disruption_changes \
+                    ON public.disruption; \
+                    CREATE TRIGGER last_disruption_changes \
+                    BEFORE UPDATE \
+                    ON public.disruption \
+                    FOR EACH ROW \
+                    EXECUTE PROCEDURE log_disruption_update();')
 
 def downgrade():
-    op.drop_table('history.associate_impact_pt_object'),
-    op.drop_table('history.associate_disruption_impact'),
-    op.drop_table('history.impact')
+    op.drop_table('associate_impact_pt_object', schema='history'),
+    op.drop_table('associate_disruption_impact', schema='history'),
+    op.drop_table('impact', schema='history')
